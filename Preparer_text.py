497b3e89420a1ca0fc75bf1664406132630bd677
Doun <daniel.esco92@gmail.com>
2022-12-25 10:25:29 +0100

    Update Preparer_text.py
    Réglé le problème de l'apostrophe

--- Preparer_text.py
+++ Preparer_text.py
@@ -17,19 +17,21 @@ class Preparation_text:
         return one_line
 
     def retirer_apostrophe(self, liste):
-        # TODO: Transformer ça en fonction
         # Comprendre ce qui se passe
         apostrophe = "’"
         # print(liste)
+        nouvelle_liste = []
         for el in liste:
             # print(el)
             if apostrophe in el:
                 # print(el)
                 sans_apostrophe = el.split(apostrophe)
-                # print(sans_apostrophe)
-                if sans_apostrophe[0] in ["d", "qu"]:
+                # On ajoute un e pour enlever l'élision
+                if sans_apostrophe[0] in ["d", "qu", "j"]:
                     sans_apostrophe[0] = sans_apostrophe[0]+"e"
-                # print(sans_apostrophe)
+                    # print(sans_apostrophe)
+                    el = ' '.join(sans_apostrophe)
+
             #     # TODO
             #     # On devrait aller dans le dictionnaire et voir si sans_apostrophe[0] est féminin ou masculin, 1er cas +a, deuxième cas +e
             #     correction = ' '.join(sans_apostrophe)
@@ -37,6 +39,15 @@ class Preparation_text:
             #     return correction
 
                 # print(correction)
+
+        # On fait un dernier test s'il y a un espace du à l'ancienne présence d'une apostrophe
+            if " " in el:
+                el_ss_apostrophe = el.split(" ")
+                for e in el_ss_apostrophe:
+                    nouvelle_liste.append(e)
+            else:
+                nouvelle_liste.append(el)
+        return nouvelle_liste
 
     def remove_punctuation(self, texte):
         ponctuation = ['.', '?', '!', ',',
@@ -54,13 +65,13 @@ class Preparation_text:
                         one_line = one_line.replace(character, '')
                     elif character in ponctuation_avec_espace:
                         one_line = one_line.replace(character, ' ')
-                # Pour l'apostrophe, le traitement es tun peu spécial puisqu'il faut savoir si le nom est masculin féminin
+                # Pour l'apostrophe, le traitement est un peu spécial puisqu'il faut savoir si le nom est masculin féminin
                 # Il ne faut donc pas séparer par caractère, mais pas "mot"
                 liste_de_mots = one_line.split(" ")
                 liste_de_mots = list(filter(None, liste_de_mots))
                 # print(liste_de_mots)
-                test = self.retirer_apostrophe(liste_de_mots)
-                # print(test)
+                text_ss_apostrophe = self.retirer_apostrophe(liste_de_mots)
+                print(text_ss_apostrophe)
         # else:
         #     for character in texte:
         #         if character in ponctuation:
